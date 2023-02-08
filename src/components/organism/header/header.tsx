4d2dae5626ab3ae6ea510704b908cd38be9e26a3
minsangkimme <minsang.tech@gmail.com>
2023-02-08 23:23:30 +0900

    add: header 구현

--- src/components/organism/header/header.tsx
+++ src/components/organism/header/header.tsx
@@ -1,30 +1,62 @@
+import { useState } from 'react';
+
 import { navPath } from '../layout/data';
 import styles from './header.module.scss';
-import Logo from "assets/images/logo/logo-yegam.svg";
+import Logo from 'assets/images/logo/logo-yegam.svg';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 
 const Header = () => {
+  const [toggle, setToggle] = useState<boolean>();
+  const handleOnClick = () => setToggle(!toggle);
+  const handleNavItem = () => {
+    handleOnClick();
+    window.scrollTo({ behavior: 'smooth', top: 0 });
+  };
+
   return (
-    <header className={clsx(styles['root'], "container-fluid")}>
-      <div className={clsx(styles['nav-wrapper'], 'container', 'py-lg-4')}>
-        <nav className="row align-items-center justify-between gy-0">
-          <Link to={'/'}>
-            <img className="mr-25" src={Logo} alt="yegam" />
+    <header className={clsx(styles['root'], 'container-fluid')}>
+      <div
+        className={clsx(
+          styles['nav-wrapper'],
+          'container',
+          'py-lg-4',
+          'px-0',
+          'py-0',
+          'px-md-4',
+          'py-md-4',
+        )}>
+        <nav className='row align-items-center justify-between gy-0 flex-column flex-md-row'>
+          <Link className={styles['logo-link']} to={'/'}>
+            <img className='ml-10 mr-10' src={Logo} alt='yegam' />
           </Link>
-          <ul className={styles['ul-wrapper']}>
+          <div className={clsx(styles['hamburger-menu-wrapper'], 'd-md-none')}>
+            <input type='checkbox' onClick={handleOnClick} checked={toggle} />
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <ul
+            className={clsx(
+              styles['ul-wrapper'],
+              toggle ? styles['active'] : '',
+            )}>
             {navPath.map((item) => {
               return (
-                <li className='black' key={item.path}>
+                <li className='black' key={item.path} onClick={handleNavItem}>
                   <Link to={item.path}>{item.name}</Link>
                 </li>
-              )
+              );
             })}
           </ul>
+          <div
+            className={toggle ? styles['backdrop'] : ''}
+            onClick={handleOnClick}
+          />
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
