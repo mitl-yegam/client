d192a058b741f6a820d4b4d6fc8e51419dc39f87
eungyeollee <eungyeol.lee@duse.co.kr>
2023-02-12 14:38:16 +0900

    add: company page

--- src/pages/company/company.tsx
+++ src/pages/company/company.tsx
@@ -27,30 +27,54 @@ const Company = () => {
             <section className="container">
                 <div className="d-center flex-column py-12">
                     <img src={IconRoundedCheck}/>
-                    <h2 className="blue-100 mt-3 mb-12">정보통신공사 면허를 가진 기업인지 꼭 확인하세요!</h2>
-                    <img src={LogoKica}/>
+                    <h2 className="blue-100 mt-3 mb-12 text-center">정보통신공사 면허를 가진 기업인지 꼭 확인하세요!</h2>
+                    <div>
+                        <img src={LogoKica} width="100%"/>
+                    </div>
                     <p className="bold3 text-center mt-12">
                         예감은 정보통신공사협회에 등록된 정보통신공사 면허를 가진 전문 기업입니다.<br/>
                         오랜 경험을 바탕으로 항상 고객의 입장에서 생각하고,<br/>
                         고객의 환경에 맞추어 저렴한 가격으로 최상의 네트워크 환경을 구축합니다.
                     </p>
                 </div>
-                <div className="d-center gap-4">
-                    <img src={Certification1}/>
-                    <img src={Certification2}/>
-                    <img src={Certification3}/>
-                    <img src={Certification4}/>
-                    <img src={Certification5}/>
+                <div className="d-flex justify-between flex-nowrap">
+                    <div>
+                        <img src={Certification1} width="100%"/>
+                    </div>
+                    <div>
+                        <img src={Certification2} width="100%"/>
+                    </div>
+                    <div>
+                        <img src={Certification3} width="100%"/>
+                    </div>
+                    <div>
+                        <img src={Certification4} width="100%"/>
+                    </div>
+                    <div>
+                        <img src={Certification5} width="100%"/>
+                    </div>
                 </div>
-                <div className="d-center flex-column pt-20 pb-25">
-                    <h2 className="blue-100 mb-12">주요고객사</h2>
-                    <div className="d-center gap-6 mb-9">
-                        <img src={LogoExample}/>
-                        <img src={LogoExample}/>
-                        <img src={LogoExample}/>
-                        <img src={LogoExample}/>
-                        <img src={LogoExample}/>
-                        <img src={LogoExample}/>
+                <div className="pt-20 pb-25">
+                    <h2 className="blue-100 mb-12 text-center">주요고객사</h2>
+                    <div className="row gx-1 mb-9">
+                        <div className="col-6 col-md-2">
+                            <img src={LogoExample} width="100%"/>    
+                        </div>
+                        <div className="col-6 col-md-2">
+                            <img src={LogoExample} width="100%"/>
+                        </div>
+                        <div className="col-6 col-md-2">
+                            <img src={LogoExample} width="100%"/>
+                        </div>
+                        <div className="col-6 col-md-2">
+                            <img src={LogoExample} width="100%"/>
+                        </div>
+                        <div className="col-6 col-md-2">
+                            <img src={LogoExample} width="100%"/>
+                        </div>
+                        <div className="col-6 col-md-2">
+                            <img src={LogoExample} width="100%"/>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -63,7 +87,7 @@ const Company = () => {
                     <div className="row">
                         <div className="col col-md-6 col-lg-4 d-center flex-column">
                             <div>
-                                <img src={NewExample} alt="" />
+                                <img src={NewExample} width="100%" alt="" />
 
                                 <p className="mt-5 bold3">2022년 여름창립기념</p>
                                 <p className="">
@@ -73,7 +97,7 @@ const Company = () => {
                         </div>
                         <div className="col col-md-6 col-lg-4 d-center flex-column">
                             <div>
-                                <img src={NewExample} alt="" />
+                                <img src={NewExample} width="100%" alt="" />
                                 <p className="mt-5 bold3">2022년 여름창립기념</p>
                                 <p className="">
                                     필리핀 바탕가스 라파지역 <br/>빈민촌 아이들 식사후원
