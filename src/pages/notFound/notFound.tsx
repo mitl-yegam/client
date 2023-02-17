88fa1bd432bf4754ee4a15b76efcea95e76f8d52
minsangkimme <minsang.tech@gmail.com>
2023-02-17 21:46:25 +0900

    add: 404 error page

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -1,8 +1,13 @@
-const NotFound = () => {
+import notFound from 'assets/images/etc/not-found.png';
 
+const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className='container'>
+      <div className='d-center py-20 py-md-0'>
+        <img src={notFound} alt='notFound' width={'100%'} />
+      </div>
+    </div>
+  );
+};
 
 export default NotFound;
